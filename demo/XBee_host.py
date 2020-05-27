99b9f0a79c469993ea035414d52c8365cf5d95e9
yvonne0520 <yvonne8805200520@gmail.com>
2020-05-27 13:50:57 +0800

    demo

---
+++ demo/XBee_host.py
@@ -0,0 +1,22 @@
+import serial
+import time
+
+# XBee setting
+serdev = '/dev/ttyUSB0'
+s = serial.Serial(serdev, 9600)
+
+s.write("+++".encode())
+s.write("ATMY <0x140>\r\n".encode())
+s.write("ATDL <0x240>\r\n".encode())
+s.write("ATID <0x1>\r\n".encode())
+s.write("ATWR\r\n".encode())
+s.write("ATMY\r\n".encode())
+s.write("ATDL\r\n".encode())
+s.write("ATCN\r\n".encode())
+
+print("start sending RPC")
+
+while True:
+    s.write("/getAcc/run\r".encode())
+    time.sleep(1)    
+s.close()
